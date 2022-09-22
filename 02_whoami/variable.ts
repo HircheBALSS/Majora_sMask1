c636089ba309cdff5ef226d6419167f61006ec05
baland_h <baland_h@etna-alternance.net>
2022-09-22 14:41:25 -0400

    exo

---
+++ 02_whoami/variable.ts
@@ -0,0 +1,38 @@
+let addition: number = 7;
+
+addition += 3;
+
+console.log(addition);
+
+const age: number = 12;
+
+let pasyears: number = 2;
+
+let solu: number = age + pasyears;
+
+console.log(solu);
+
+pasyears = 14;
+
+solu = age + pasyears;
+
+console.log(solu);
+
+solu -= 10;
+console.log(solu);
+
+solu *= 5;
+console.log(solu);
+solu/= 2;
+
+console.log(solu);
+
+const numberAppel: number = 50;
+const totalAppel: number = numberAppel %3;
+const givenAppel: number = numberAppel /6;
+
+console.log(givenAppel);
+console.log(totalAppel);
+
+console.log('the person will receved ${givenAppel} appel.');
+console.log('the person will receved ${totalAppel} appel.');
